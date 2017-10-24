987ed6065c0554892893af5040cc18c186e28b5d
Esther Dalley <estherdalley@users.noreply.github.com>
2017-10-24 22:18:26 +0100

    CameraAudio: 1-6 implemented

---
+++ SpyApp/app/src/main/java/com/example/estherdalley/spyapp/CameraAudio.java
@@ -0,0 +1,13 @@
+package com.example.estherdalley.spyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraAudio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_audio);
+    }
+}
